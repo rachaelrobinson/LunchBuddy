82aab09a9831a5ff90b75de6b5545332420150bc
Owen Huyn <huynowen@gmail.com>
2017-07-26 11:00:32 -0700

    Added algorithm.

--- server.py
+++ server.py
@@ -102,31 +102,31 @@ def register():
 
 @app.route('/reserve', methods=['GET', 'POST'])
 def reserve():
-	if request.method == 'GET':
-		if session.get('logged_in'):
-			return render_template("reserve.html")
-		else:
-			return render_template('login.html')
-		# similar format to register
-		# __name__
-		# campus options
-		# schedule
-	else:
-		print request.form['date']
-		print request.form['pcampus']
-		print request.form['stime']
-		# Things you get back from form:
-			#pcampus: (N, E, W, B)
-			#scampus: (N, E, W, B)
-			#ptime: (0, 1, 2) 0=11-12, 1=12-1, 2=1-2
-			#stime: (0, 1, 2)
-			#date: (YYYY-MM-DD)
-		# if successful add to DB:
-		return jsonify([{'status':200}])
-	# similar format to register
-	# __name__
-	# campus options
-	# schedule
+    if request.method == 'GET':
+        if session.get('logged_in'):
+            return render_template("reserve.html")
+        else:
+            return render_template('login.html')
+        # similar format to register
+        # __name__
+        # campus options
+        # schedule
+    else:
+        print request.form['date']
+        print request.form['pcampus']
+        print request.form['stime']
+        # Things you get back from form:
+            #pcampus: (N, E, W, B)
+            #scampus: (N, E, W, B)
+            #ptime: (0, 1, 2) 0=11-12, 1=12-1, 2=1-2
+            #stime: (0, 1, 2)
+            #date: (YYYY-MM-DD)
+        # if successful add to DB:
+        return jsonify([{'status':200}])
+    # similar format to register
+    # __name__
+    # campus options
+    # schedule
 
 @app.route('/logout', methods=['GET'])
 def logout():
@@ -137,60 +137,113 @@ def logout():
 
 @app.route('/profile')
 def profile():
-	print "YA GOT IT"
-	#dummy data to test profiles
-	email = 'rachael@mcrsft.com'
-	reservations = [{'reservation':'July 25th, 1-2pm, North Campus', 'status':'scheduled'}, {'reservation':'July 30th, 1-2pm, West Campus', 'status':'pending'}]
-	name = 'rachaelrobinson'
-	person = {'name': name, 'email': email}
-	print person
-	#TODO: send username
-	return render_template("profile.html", user=person)
-	#display info from registration db, along w/ scheduled dates
-	# username is just email
-	pass
-	# if not session.get('logged_in') or not session.get('user'):
-	# 	return redirect(url_for('home'))
-	# if session.get('user') != username:
-	# 	return redirect(url_for('home'))
-	# else:
-	# 	print "YA GOT IT"
-	# 	#dummy data to test profiles
-	# 	email = 'rachael@mcrsft.com'
-	# 	reservations = [{reservation:'July 25th, 1-2pm, North Campus', status:'scheduled'}, {reservation:'July 30th, 1-2pm, West Campus', status:'pending'}]
-	# 	name = 'rachaelrobinson'
-	# 	#TODO: send username
-	# 	return render_template("profile.html", email=email, reservations=reservations, name=name)
-	# #display info from registration db, along w/ scheduled dates
-	# # username is just email
-	# pass
+    print "YA GOT IT"
+    #dummy data to test profiles
+    email = 'rachael@mcrsft.com'
+    reservations = [{'reservation':'July 25th, 1-2pm, North Campus', 'status':'scheduled'}, {'reservation':'July 30th, 1-2pm, West Campus', 'status':'pending'}]
+    name = 'rachaelrobinson'
+    person = {'name': name, 'email': email}
+    print person
+    #TODO: send username
+    return render_template("profile.html", user=person)
+    #display info from registration db, along w/ scheduled dates
+    # username is just email
+    pass
+    # if not session.get('logged_in') or not session.get('user'):
+    # 	return redirect(url_for('home'))
+    # if session.get('user') != username:
+    # 	return redirect(url_for('home'))
+    # else:
+    # 	print "YA GOT IT"
+    # 	#dummy data to test profiles
+    # 	email = 'rachael@mcrsft.com'
+    # 	reservations = [{reservation:'July 25th, 1-2pm, North Campus', status:'scheduled'}, {reservation:'July 30th, 1-2pm, West Campus', status:'pending'}]
+    # 	name = 'rachaelrobinson'
+    # 	#TODO: send username
+    # 	return render_template("profile.html", email=email, reservations=reservations, name=name)
+    # #display info from registration db, along w/ scheduled dates
+    # # username is just email
+    # pass
 
-@app.route('/lunchform', methods=['POST'])
-def lunchImport():
-    if 'campus' in request.form and 'bday' in request.form and 'ptime' in request.form and 'stime' in request.form:
-        primaryid = request.form['campus'] + request.form["bday"] + request.form["ptime"]
-        secondaryid = request.form['campus'] + request.form["bday"] + request.form["stime"]
+@app.route('/timeslot', methods=['POST'])
+def saveTimeslot():
+    if 'campus' in request.form and 'bday' in request.form and 'ptime' in request.form:
+        # id: {campus}{time}-{date}
+        primaryid = '{}{}-{}'.format(request.form['campus'], request.form["ptime"], request.form["bday"])
 
         primaryentry = mongo1.db.find_one({"_id": primaryid})
         if (primaryentry is None):
+            # add document for time/date/location if it doesn't exist
             primarydate = {"_id" : primaryid, "users" : [session['user']]}
-            mongo1.db.primary_time.insert_one(data)
+            mongo1.db.timeslots.insert_one(data)
         else:
             primaryentry['users'].append(session['user'])
-            mongo1.db.primary_time.update_one(data)
-
-        secondarydate = {"_id" : secondaryid, "users" : [session['user']]}
-        if (secondarydate is None):
-            primarydate = {"_id" : secondaryid, "users" : [session['user']]}
-            mongo1.db.primary_time.insert_one(data)
-        else:
-            primaryentry['users'].append(session['user'])
-            mongo1.db.primary_time.update_one(data)
+            # update existing document
+            mongo1.db.timeslots.update_one(data)
 
         return jsonify([{'status':200}])
     else:
         flash('Missing fields!')
         return jsonify([{'status':400}])
+
+# to be run periodically, not a route call
+def matchBuddies(date):
+    # go through in each campus + time
+    locations = ['W','E','B','N']
+    times = ['1','2','3']
+    #TODO : improve this nested for loop to only go through documents that exist on the database
+    for location in locations:
+        for time in times:
+            # find an appropriate timeslot in the database
+            id = '{}{}-{}'.format(location,time,date)
+            timeslot = mongo1.db.timeslots.find_one(id)
+
+            # if document exists, there are people
+            if (timeslot is not None):
+                # if user is alone in that timeslot, don't group them
+                userCount = len(timeslot['users'])
+                if (userCount == 1):
+                    # TODO: send an email
+                    print 'Send email'
+                else:
+                    users = timeslot['users']
+                    # distribute to separate groups                    
+                    matchedUsers = distributeUsers(users)
+                    
+                    # go through each user and insert a meeting containing the list of people
+                    for i in range(len(matchedUsers)):
+                        data = { '_id' : id, 'users' : matchedUsers[i] }
+                        m_id = mongo1.db.meetings.insert_one(data)
+                        # update user with the current m_id
+                        for username in matchedUsers[i]:
+                            user = mongo1.db.users.find_one(username)
+                            # set a meeting ID for the associated user and update the database
+                            user.m_id = m_id
+                            mongo1.db.users.update_one(user)
+                
+                # TODO: delete previous entry
+
+            # else just continue, not necessary just done for readability
+            else:
+                continue
+
+def distributeUsers(users):
+    matchedUsers = None
+    if (userCount % 6 == 0):
+        matchedUsers = initUserArr(userCount/6)
+    else:
+        # +1 since 7 would mean two groups, while 6 would mean one group
+        matchedUsers = initUserArr(userCount/6 + 1)
+    # distribute each user to appropriate bucket by dividing in 6
+    for i in range(len(users)):
+        matchedUsers[i % len(matchedUsers)].append(users[i])
+    return matchedUsers
+
+def initUserArr(count):
+    arr = []
+    for i in range(count):
+        arr.append([])
+    return arr
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
